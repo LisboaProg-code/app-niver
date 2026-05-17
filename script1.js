78266f890445c1a59c71b872f4fe16169a06e002
Lisboa <lisboaneto56@gmail.com>
2026-05-17 15:55:47 -0300

    senha

--- script1.js
+++ script1.js
@@ -72,7 +72,7 @@ function subirTelaLock() {
 }
 
 // --- CONFIGURAÇÃO DA SENHA DE 6 DÍGITOS ---
-const SENHA_CORRETA = "160408"; 
+const SENHA_CORRETA = "160508"; 
 let senhaDigitada = "";
 
 function pressKey(num) {
